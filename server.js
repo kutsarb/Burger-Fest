a8da25acd4368ba699132132e9c74af77a4bfb9f
kutsarb <68346526+kutsarb@users.noreply.github.com>
2021-02-12 14:15:37 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ var PORT = process.env.PORT || 3000;
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
-
+app.use(express.static(__dirname + '/public'));
 // Parse application body
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
