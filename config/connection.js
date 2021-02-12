fd8771f9fd96349fae747bca11f010015d2e6563
kutsarb <68346526+kutsarb@users.noreply.github.com>
2021-02-12 13:49:42 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "",
   port: 3306,
-  user: "root",
-  password: "root",
+  user: "",
+  password: "",
   database: "burgers_db"
 });
 
